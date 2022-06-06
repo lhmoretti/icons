d3534b809ed06efe4f91724b13f5abec04864c66
Lucas Moretti <morettilucash@gmail.com>
2022-06-06 14:17:55 -0300

    icons

---
+++ icons/index.js
@@ -0,0 +1,67 @@
+import jsIcon from './javascript.svg'
+import tsIcon from './typescript.svg'
+import javaIcon from './java.svg'
+import kotlinIcon from './kotlin.svg'
+import ionicIcon from './ionic.svg'
+import angularIcon from './angular.svg'
+import reactNativeIcon from './react-native.svg'
+import androidIcon from './android.svg'
+import reactIcon from './react.svg'
+import springIcon from './spring.svg'
+import nodejsIcon from './nodejs.svg'
+import bitbucketIcon from './bitbucket.svg'
+import mongodbIcon from './mongodb.svg'
+import mysqlIcon from './mysql.svg'
+import nginxIcon from './nginx.svg'
+import npmIcon from './npm.svg'
+import postgresqlIcon from './postgresql.svg'
+import ubuntuIcon from './ubuntu.svg'
+import linkedinIcon from './linkedin.png'
+import githubIcon from './github.png'
+import downloadIcon from './download.png'
+
+const javascript = jsIcon
+const typescript = tsIcon
+const java = javaIcon
+const kotlin = kotlinIcon
+const ionic = ionicIcon
+const angular = angularIcon
+const reactNative = reactNativeIcon
+const android = androidIcon
+const react = reactIcon
+const spring = springIcon
+const nodejs = nodejsIcon
+const bitbucket = bitbucketIcon
+const mongodb = mongodbIcon
+const mysql = mysqlIcon
+const nginx = nginxIcon
+const npm = npmIcon
+const postgresql = postgresqlIcon
+const ubuntu = ubuntuIcon
+const linkedin = linkedinIcon
+const github = githubIcon
+const download = downloadIcon
+
+export const allIcons = {
+    javascript,
+    typescript,
+    java,
+    kotlin,
+    ionic,
+    angular,
+    reactNative,
+    android,
+    react,
+    spring,
+    nodejs,
+    bitbucket,
+    github,
+    mongodb,
+    mysql,
+    nginx,
+    npm,
+    postgresql,
+    ubuntu,
+    linkedin,
+    download,
+}
